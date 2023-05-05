0ee8e3b9e69582282dbe1973474f3484cf94f740
Windson <windson2ivan@outlook.com>
2023-05-05 09:39:20 +0800

    fix #28

--- src/html/Serve.ts
+++ src/html/Serve.ts
@@ -1,18 +1,18 @@
-import type {HtmlPluginOptions} from "./types"
-import {Base} from "./Base"
-import {buildHistoryApiFallback} from "../history-api/historyApiFallbackPlugin"
-import type {ViteDevServer} from "vite"
-import type {HistoryApiOptions, HistoryRewrites} from "../history-api/types"
-import {normalizePath} from "./utils"
+import type { HtmlPluginOptions } from './types'
+import { Base } from './Base'
+import { buildHistoryApiFallback } from '../history-api/historyApiFallbackPlugin'
+import type { ViteDevServer } from 'vite'
+import type { HistoryApiOptions, HistoryRewrites } from '../history-api/types'
+import { normalizePath } from './utils'
 
 export class Serve extends Base {
   _rewrites?: Array<HistoryRewrites>
-
+  
   constructor(virtualHtmlOptions: HtmlPluginOptions & HistoryApiOptions) {
     super(virtualHtmlOptions)
     this._rewrites = virtualHtmlOptions.rewrites
   }
-
+  
   _configureServer = (server: ViteDevServer) => {
     if (this._rewrites) {
       buildHistoryApiFallback(server, this._rewrites)
@@ -20,7 +20,9 @@ export class Serve extends Base {
     // other html handled after vite's inner middlewares.
     return () => {
       server.middlewares.use('/', async (req, res, next) => {
-        let url = decodeURI(this.generateUrl(req.originalUrl))
+        const originalUrl = req.originalUrl
+        const reqUrl = req.url
+        let url = decodeURI(this.generateUrl(originalUrl?.endsWith('/') ? originalUrl : reqUrl))
         // if request is not html , directly return next()
         if ((!url.endsWith('.html') && !url.endsWith('/')) && url !== '/') {
           return next()
@@ -32,7 +34,7 @@ export class Serve extends Base {
           url = `/${this._indexPage}.html`
         }
         // load specify html file code
-        htmlCode = await this._load(normalizePath(url))??''
+        htmlCode = await this._load(normalizePath(url)) ?? ''
         const transformResult = await this._transform(htmlCode, url)
         if (transformResult === null) {
           return next()
